fab3f1d2bfffef6c2e6bffd929260ec5646b78e9
Túlio Vieira <72238565+CachorroLouco7@users.noreply.github.com>
2022-09-04 12:20:42 -0300

    Add files via upload

---
+++ burffouinerf.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int a, b, c, d;
+
+    cin>>a>>b>>c>>d;
+
+    if((a*b)>(c*d)){
+        cout<<"NERF";
+    }else{
+        cout<<"BUFF";
+    }
+
+   
+}
